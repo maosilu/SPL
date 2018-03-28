ea9c19f14aad87c5efd07dbc5066ce32fa39076e
maosilu <1540688711@qq.com>
2018-03-28 11:55:47 +0800

    rewind与current操作

--- SplDoublyLinkedList.php
+++ SplDoublyLinkedList.php
@@ -8,4 +8,7 @@ $dlist->push(3);
 // unshift把新的节点数据添加到链表的底部（bottom）
 $dlist->unshift(10);
 print_r($dlist);
+// rewind操作用于把节点指针指向bottom所在的节点
+$dlist->rewind();
+// current指向当前节点，要与rewind搭配
 echo "当前节点current: ".$dlist->current();
