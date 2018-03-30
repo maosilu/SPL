a79e90a98f14bc090e7af8b828745b89ed6ecd93
maosilu <1540688711@qq.com>
2018-03-30 17:00:09 +0800

    Countable

---
+++ Countable08.php
@@ -0,0 +1,8 @@
+<?php
+$array = array(
+	array('name' => 'Jonathan', 'id' => 5),
+	array('name' => 'Peter', 'id' => 6),
+	array('name' => 'Lisa', 'id' => 7)
+);
+echo count($array)."\r\n";
+echo count($array[1])."\r\n";
--- FilesystemIterator07.php
+++ FilesystemIterator07.php
@@ -1,6 +1,6 @@
 <?php
 // FilesystemIterator能遍历文件系统
-date_default_timezone_set('PRC');
+date_default_timezone_set('PRC'); // PRC: The People's Republic of China
 $it = new FilesystemIterator('.');
 foreach ($it as $finfo) {
 	// echo date("Y-m-d H:i:s", $finfo->getMTime())."\r\n";
