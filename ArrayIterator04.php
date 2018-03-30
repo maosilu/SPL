627ec7c71e2fa8561dd59f49aa4c12850e8e9a13
maosilu <1540688711@qq.com>
2018-03-30 11:30:13 +0800

    迭代器的使用

--- ArrayIterator04.php
+++ ArrayIterator04.php
@@ -21,3 +21,29 @@ while ($it->valid()) {
 	echo $it->key()." : ".$it->current()."\r\n";
 	$it->next(); // 这句千万不能少
 }
+
+// Iterator的好处：跳过某些元素进行打印
+echo "\r\n";
+$it->rewind();
+if($it->valid()){
+	$it->seek(1);
+	while ($it->valid()) {
+		echo $it->key().' : '.$it->current()."\r\n";
+		$it->next();
+	}
+}
+
+echo "\r\n";
+echo "***** use ksort *****\r\n";
+$it->ksort();
+foreach($it as $key=>$value){
+	echo $key.' : '.$value."\r\n";
+}
+
+
+echo "\r\n";
+echo "***** use asort *****\r\n";
+$it->asort();
+foreach($it as $key=>$value){
+	echo $key.' : '.$value."\r\n";
+}
