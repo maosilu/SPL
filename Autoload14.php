40b776d5b42fec539543b8d5b58a725d000fa941
maosilu <1540688711@qq.com>
2018-04-05 13:39:02 +0800

    SPL的autoload机制的注释修改

--- Autoload14.php
+++ Autoload14.php
@@ -1,5 +1,6 @@
 <?php
-spl_autoload_extensions('.class.php'); // 找扩展名是 .class.php 的文件
-set_include_path(get_include_path().PATH_SEPARATOR.'libs/'); // 在 libs 文件下寻找
-spl_autoload_register(); // 告诉php需要使用autoload这种方法
+spl_autoload_extensions('.class.php, .php'); // 设置autoload寻找php定义的类文件的扩展名，多个扩展名用逗号分隔，前面的扩展名优先被匹配
+set_include_path(get_include_path().PATH_SEPARATOR.'libs/'); // 设置autoload寻找php定义的类文件的目录，多个目录用PATH_SEPARATOR进行分隔
+spl_autoload_register(); // 提示PHP使用Autoload机制查找类定义
 new Test13();
+// new Test12();
